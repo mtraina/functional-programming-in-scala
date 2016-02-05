9cdd37e8b9dfd71e3afdad67ba152a5d0c98fecb
mtraina <matteo.traina.mail@gmail.com>
2016-02-05 11:55:06 +0100

    ex 2.1, fibonacci part II

--- src/main/scala/com/mtraina/fpis/chapter02/Ex2_1.scala
+++ src/main/scala/com/mtraina/fpis/chapter02/Ex2_1.scala
@@ -1,9 +1,17 @@
 package com.mtraina.fpis.chapter02
 
+import scala.annotation.tailrec
+
 class Ex2_1 {
 
   def fib(n: Int): Int = {
-    1
+    @tailrec
+    def go(n: Int, acc: Int, sum: Int): Int = {
+      if(n == 0) acc
+      else if(n == 1) sum
+      else go(n-1, sum, acc+sum)
+    }
+    go(n, 0, 1)
   }
 
 }
--- src/test/scala/com/mtraina/fpis/chapter02/Ex2_1Spec.scala
+++ src/test/scala/com/mtraina/fpis/chapter02/Ex2_1Spec.scala
@@ -6,6 +6,12 @@ class Ex2_1Spec extends FlatSpec with Matchers {
 
   "A Fibonacci calculation" should "return" in {
     val ex2_1 = new Ex2_1
+    ex2_1.fib(0) should be (0)
     ex2_1.fib(1) should be (1)
+    ex2_1.fib(2) should be (1)
+    ex2_1.fib(3) should be (2)
+    ex2_1.fib(4) should be (3)
+    ex2_1.fib(5) should be (5)
+    ex2_1.fib(10) should be (55)
   }
 }
