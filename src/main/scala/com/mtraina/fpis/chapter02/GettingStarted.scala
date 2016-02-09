599cdc77171048fe7d7beaf6ed0b08e117582ea2
mtraina <matteo.traina.mail@gmail.com>
2016-02-09 17:57:27 +0100

    partial, curry, uncurry and compose to finish chapter 2

--- src/main/scala/com/mtraina/fpis/chapter02/GettingStarted.scala
+++ src/main/scala/com/mtraina/fpis/chapter02/GettingStarted.scala
@@ -67,4 +67,29 @@ object GettingStarted {
 
     loop(0)
   }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = {
+    (b: B) => f(a, b)
+  }
+
+  /**
+    * Ex. 2.3
+    */
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    a => b => f(a, b)
+  }
+
+  /**
+    * Ex. 2.4
+    */
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  /**
+    * Ex. 2.5
+    */
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
 }
