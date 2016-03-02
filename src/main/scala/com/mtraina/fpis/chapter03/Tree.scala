ebfd4a1ebb2932fb05d6455c55fe2aca7537fef0
mtraina <matteo.traina.mail@gmail.com>
2016-03-02 15:41:52 +0100

    fold function

--- src/main/scala/com/mtraina/fpis/chapter03/Tree.scala
+++ src/main/scala/com/mtraina/fpis/chapter03/Tree.scala
@@ -54,4 +54,10 @@ object Tree {
     }
   }
 
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = {
+    t match {
+      case Leaf(v) => f(v)
+      case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    }
+  }
 }
