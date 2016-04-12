1c7586629f5b7c5d63a92163f0c85b6172a7261c
mtraina <matteo.traina.mail@gmail.com>
2016-04-12 15:46:23 +0200

    map impl 1

--- src/test/scala/com/mtraina/fpis/chapter04/OptionSpec.scala
+++ src/test/scala/com/mtraina/fpis/chapter04/OptionSpec.scala
@@ -4,4 +4,8 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class OptionSpec extends FlatSpec with Matchers {
 
+  it should "map" in {
+    val o = new Some[Int](5)
+    o.map(x => x * 2) shouldBe None
+  }
 }
