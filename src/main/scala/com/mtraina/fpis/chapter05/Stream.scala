3a9fa41ffdca98315de36c961ffb6e378cb5b7d8
mtraina <matteo.traina.mail@gmail.com>
2016-04-19 15:53:10 +0200

    lazyness: stream impl II

--- src/main/scala/com/mtraina/fpis/chapter05/Stream.scala
+++ src/main/scala/com/mtraina/fpis/chapter05/Stream.scala
@@ -1,6 +1,13 @@
 package com.mtraina.fpis.chapter05
 
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+}
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
