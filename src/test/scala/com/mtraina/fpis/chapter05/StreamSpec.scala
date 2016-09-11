16ffa87436c7fa34154fe160bd81f0adec945728
mtraina <matteo.traina.mail@gmail.com>
2016-09-11 21:15:03 +0200

    stream refactoring

--- src/test/scala/com/mtraina/fpis/chapter05/StreamSpec.scala
+++ src/test/scala/com/mtraina/fpis/chapter05/StreamSpec.scala
@@ -7,13 +7,13 @@ class StreamSpec extends FlatSpec with Matchers {
 
   "A Stream" should "return the optional head" in {
     Empty.headOption shouldBe None
-    cons(2, cons(4, cons(1, Empty))).headOption shouldBe Some(2)
+    Stream(2, 4, 1).headOption shouldBe Some(2)
 
     /**
       * Ex. 5.6
       */
     Empty.headOption2 shouldBe None
-    cons(2, cons(4, cons(1, Empty))).headOption2 shouldBe Some(2)
+    Stream(2, 4, 1).headOption2 shouldBe Some(2)
   }
 
   /**
@@ -24,51 +24,51 @@ class StreamSpec extends FlatSpec with Matchers {
   }
 
   it should "convert to a list when operating on a Cons" in {
-    cons(1, Empty).toList shouldBe List(1)
-    cons(1, cons(2, cons(3, Empty))).toList shouldBe List(1,2,3)
+    Stream(1).toList shouldBe List(1)
+    Stream(1, 2, 3).toList shouldBe List(1,2,3)
   }
 
   /**
     * Ex. 5.2
     */
   it should "return the first n elements of a Stream" in {
-    cons(1, Empty).take(1).toList shouldBe List(1)
-    cons(1, cons(2, cons(3, Empty))).take(2).toList shouldBe List(1,2)
+    Stream(1).take(1).toList shouldBe List(1)
+    Stream(1, 2, 3).take(2).toList shouldBe List(1,2)
   }
 
   it should "return the last n elements of a Stream" in {
-    cons(1, Empty).drop(1).toList shouldBe empty
-    cons(1, cons(2, cons(3, Empty))).drop(2).toList shouldBe List(3)
+    Stream(1).drop(1).toList shouldBe empty
+    Stream(1, 2, 3).drop(2).toList shouldBe List(3)
   }
 
   /**
     * Ex. 5.3
     */
   it should "return all starting elements matching the predicate" in {
-    cons(2, cons(4, cons(1, Empty))).takeWhile(x => x % 2 == 0).toList shouldBe List(2,4)
-    cons(1, cons(2, cons(3, cons(4, cons(5, Empty))))).takeWhile(x => x < 3).toList shouldBe List(1, 2)
-    cons(1, cons(2, cons(3, cons(4, cons(5, Empty))))).takeWhile(x => x > 3).toList shouldBe List()
+    Stream(2, 4, 1).takeWhile(x => x % 2 == 0).toList shouldBe List(2,4)
+    Stream(1, 2, 3, 4, 5).takeWhile(x => x < 3).toList shouldBe List(1, 2)
+    Stream(1, 2, 3, 4, 5).takeWhile(x => x > 3).toList shouldBe List()
 
     /**
       * Ex. 5.5
       */
-    cons(2, cons(4, cons(1, Empty))).takeWhile2(x => x % 2 == 0).toList shouldBe List(2,4)
-    cons(1, cons(2, cons(3, cons(4, cons(5, Empty))))).takeWhile2(x => x < 3).toList shouldBe List(1, 2)
-    cons(1, cons(2, cons(3, cons(4, cons(5, Empty))))).takeWhile2(x => x > 3).toList shouldBe List()
+    Stream(2, 4, 1).takeWhile2(x => x % 2 == 0).toList shouldBe List(2,4)
+    Stream(1, 2, 3, 4, 5).takeWhile2(x => x < 3).toList shouldBe List(1, 2)
+    Stream(1, 2, 3, 4, 5).takeWhile2(x => x > 3).toList shouldBe List()
   }
 
   it should "check if an element matching the predicate exists" in {
-    cons(2, cons(4, cons(1, Empty))).exists(x => x % 2 == 0) shouldBe true
-    cons(2, cons(4, cons(1, Empty))).exists(x => x == 0) shouldBe false
+    Stream(2, 4, 1).exists(x => x % 2 == 0) shouldBe true
+    Stream(2, 4, 1).exists(x => x == 0) shouldBe false
   }
 
   /**
     * Ex. 5.4
     */
   it should "check if all the elements match the predicate" in {
-    cons(2, cons(4, cons(1, Empty))).forAll(x => x % 2 == 0) shouldBe false
-    cons(2, cons(4, cons(8, Empty))).forAll(x => x % 2 == 0) shouldBe true
-    cons(2, cons(4, cons(1, Empty))).forAll(x => x > 0) shouldBe true
+    Stream(2, 4, 1).forAll(x => x % 2 == 0) shouldBe false
+    Stream(2, 4, 8).forAll(x => x % 2 == 0) shouldBe true
+    Stream(2, 4, 1).forAll(x => x > 0) shouldBe true
   }
 
   /**
@@ -76,15 +76,15 @@ class StreamSpec extends FlatSpec with Matchers {
     */
   it should "implement map, filter, append and flat map using fold right" in {
     // map
-    cons(1, cons(2, cons(3, Empty))).map(x => x + 1).toList shouldBe List(2, 3, 4)
+    Stream(1, 2, 3).map(x => x + 1).toList shouldBe List(2, 3, 4)
 
     // filter
-    cons(1, cons(2, cons(3, Empty))).filter(x => x % 3 == 0).toList shouldBe List(3)
+    Stream(1, 2, 3).filter(x => x % 3 == 0).toList shouldBe List(3)
 
     // append
-    cons(1, cons(2, Empty)).append(cons(3, Empty)).toList shouldBe List(1, 2, 3)
+    Stream(1, 2).append(cons(3, Empty)).toList shouldBe List(1, 2, 3)
 
     // flat map
-    cons(3, cons(4, Empty)).flatMap(x => cons(x * 2, Empty)).toList shouldBe List(6, 8)
+    Stream(3, 4).flatMap(x => cons(x * 2, Empty)).toList shouldBe List(6, 8)
   }
 }
