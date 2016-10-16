417cba81eec044341058fdfe96c195d036035d03
mtraina <matteo.traina.mail@gmail.com>
2016-10-16 21:05:38 +0200

    ex 6.2

--- src/main/scala/com/mtraina/fpis/chapter06/State.scala
+++ src/main/scala/com/mtraina/fpis/chapter06/State.scala
@@ -6,10 +6,10 @@ trait RNG {
 
 object RNG {
 
-  case class Simple(seed: Long) extends RNG {
+  case class SimpleRNG(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-      val nextRNG = Simple(newSeed)
+      val nextRNG = SimpleRNG(newSeed)
       val n = (newSeed >>> 16).toInt
       (n, nextRNG)
     }
@@ -18,6 +18,11 @@ object RNG {
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, r) = rng.nextInt
     if(i < 0) (-i - 1, r) else (i, r)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
   }
 
 }
--- src/test/scala/com/mtraina/fpis/chapter06/StateSpec.scala
+++ src/test/scala/com/mtraina/fpis/chapter06/StateSpec.scala
@@ -1,19 +1,31 @@
 package com.mtraina.fpis.chapter06
 
-import com.mtraina.fpis.chapter06.RNG.Simple
+import com.mtraina.fpis.chapter06.RNG.SimpleRNG
 import org.scalatest.{FlatSpec, Matchers}
 
 class StateSpec extends FlatSpec with Matchers {
 
+  /**
+    * Ex. 6.1
+    */
   "A RNG" should "generate a non negative integer" in {
-    val rng = Simple(3)
+    val rng = SimpleRNG(3)
     RNG.nonNegativeInt(rng) shouldBe rng.nextInt
 
-    val rng2 = Simple(Int.MinValue).nextInt
-    if(rng2._1 > 0){
-      RNG.nonNegativeInt(Simple(Int.MinValue)) shouldBe (rng2._1, rng2._2)
+    val rng2 = SimpleRNG(Int.MinValue).nextInt
+    if(rng2._1 >= 0){
+      RNG.nonNegativeInt(SimpleRNG(Int.MinValue)) shouldBe (rng2._1, rng2._2)
     } else {
-      RNG.nonNegativeInt(Simple(Int.MinValue)) shouldBe (-rng2._1 - 1, rng2._2)
+      RNG.nonNegativeInt(SimpleRNG(Int.MinValue)) shouldBe (-rng2._1 - 1, rng2._2)
     }
+  }
+
+  /**
+    * Ex. 6.2
+    */
+  it should "generate a double between 0 and 1 not included" in {
+    val rng = SimpleRNG(1)
+    val d = RNG.double(rng)
+    d._1 >= 0d && d._1 < 1 shouldBe true
   }
 }
