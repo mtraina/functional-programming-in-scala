aec501361005f92eea16f7f9372b12c1927c900c
mtraina <matteo.traina.mail@gmail.com>
2017-06-26 10:15:25 +0100

    map2 and sum3 added

--- src/main/scala/com/mtraina/fpis/chapter07/Par.scala
+++ src/main/scala/com/mtraina/fpis/chapter07/Par.scala
@@ -6,6 +6,7 @@ import language.implicitConversions
 import com.mtraina.fpis.chapter07.Par.Par
 
 object Examples {
+
   def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
     if (ints.size <= 1)
       ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
@@ -23,6 +24,14 @@ object Examples {
       val sumR: Par[Int] = Par.unit(sum(r))
       Par.get(sumL) + Par.get(sumR)
     }
+
+  def sum3(ints: IndexedSeq[Int]): Par[Int] =
+    if (ints.size <= 1)
+      Par.unit(ints.headOption getOrElse 0)
+    else {
+      val (l,r) = ints.splitAt(ints.length/2)
+      Par.map2(sum3(l), sum3(r))(_ + _)
+    }
 }
 
 object Par {
@@ -31,6 +40,13 @@ object Par {
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
 
   def get[A](parA: Par[A]): A = ???
+
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+    }
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
