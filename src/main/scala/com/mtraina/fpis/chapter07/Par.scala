93a4b43a585bae3e33d9b112df55f42f8e732066
mtraina <matteo.traina.mail@gmail.com>
2017-06-26 09:12:00 +0100

    sum2 and par added

--- src/main/scala/com/mtraina/fpis/chapter07/Par.scala
+++ src/main/scala/com/mtraina/fpis/chapter07/Par.scala
@@ -1,5 +1,10 @@
 package com.mtraina.fpis.chapter07
 
+import java.util.concurrent._
+import language.implicitConversions
+
+import com.mtraina.fpis.chapter07.Par.Par
+
 object Examples {
   def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
     if (ints.size <= 1)
@@ -8,4 +13,30 @@ object Examples {
       val (l,r) = ints.splitAt(ints.length/2) // Divide the sequence in half using the `splitAt` function.
       sum(l) + sum(r) // Recursively sum both halves and add the results together.
     }
+
+  def sum2(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
+    else {
+      val (l,r) = ints.splitAt(ints.length/2)
+      val sumL: Par[Int] = Par.unit(sum(l))
+      val sumR: Par[Int] = Par.unit(sum(r))
+      Par.get(sumL) + Par.get(sumR)
+    }
+}
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
+
+  def get[A](parA: Par[A]): A = ???
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
 }
